32fafad9a6a8925ea55ff8b441e7cda5abeb3595
mrnegativex <rashadul99924@gmail.com>
2022-05-29 04:00:17 +0600

    jwt system addded

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -9,13 +9,25 @@ const jwt = require("jsonwebtoken");
 // medal ware
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+ 
+// middle tiear 
+const verifyJWT = (req, res, next) => {
+  const authH = req.headers.authorization
+  if(!authH){
+    return res.status(401).send({scam: true})
+  }
+  const token = authH.split(' ')[1]
+  jwt.verify(token, process.env.DB_ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({scam: true})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 // mongo
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvszi.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvszi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -29,69 +41,81 @@ const run = async () => {
     const orderCollection = client.db("tooler").collection("order");
     const userCollection = client.db("tooler").collection("user");
     const reviewCollection = client.db("tooler").collection("review");
-    
-    app.get('/tools', async(req, res)=>{
-        const result = await toolerCollection.find({}).toArray()
-        res.send(result)
-    })
-    // find by id 
-    app.get('/tools/:id', async(req, res)=>{
+
+    app.get("/tools", async (req, res) => {
+      const result = await toolerCollection.find({}).toArray();
+      res.send(result);
+    });
+    // find by id
+    app.get("/tools/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) };
       const result = await toolerCollection.findOne(query);
-      res.send(result)
-    })
-    // update available quantaty 
-    app.put('/tools/:id', async(req, res)=>{
+      res.send(result);
+    });
+    // update available quantaty
+    app.put("/tools/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const updateItem = req?.body;
       console.log(updateItem);
-      const option = {upsert: true}
+      const option = { upsert: true };
       const updateDoc = {
-        $set:{
-          available: updateItem?.newAvailable
-        }
-      }
+        $set: {
+          available: updateItem?.newAvailable,
+        },
+      };
       const result = await toolerCollection.updateOne(query, updateDoc, option);
-      res.send(result)
-
-    })
-    // orders 
-    app.post('/order', async(req, res)=>{
+      res.send(result);
+    });
+    // orders
+    app.post("/order", async (req, res) => {
       const order = req.body;
-      const result = await orderCollection.insertOne(order)
-      res.send(result)
-    })
-    // get order 
-    app.get('/order', async (req,res)=>{
-      const result = await orderCollection.find({}).toArray()
-      res.send(result)
-    })
-    // review 
-    app.post('/review', async(req, res)=>{
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    // get order
+    app.get("/order", async (req, res) => {
+      const result = await orderCollection.find({}).toArray();
+      res.send(result);
+    });
+    // get orders of one user 
+    app.get("/order/query", verifyJWT, async (req, res) => {
+      const query = req.query.email
+      const filter = {email: query}
+      const result = await orderCollection.find(filter).toArray();
+      res.send(result);
+    });
+    // review
+    app.post("/review", async (req, res) => {
       const order = req.body;
-      const result = await reviewCollection.insertOne(order)
-      res.send(result)
-    })
-    // get review 
-    app.get('/review', async (req,res)=>{
-      const result = await reviewCollection.find({}).toArray()
-      res.send(result)
-    })
+      const result = await reviewCollection.insertOne(order);
+      res.send(result);
+    });
+    // get review
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find({}).toArray();
+      res.send(result.reverse());
+    });
     // user
-    app.put('/users/:email', async(req, res)=>{
+    app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
-      const user = req.body
-      const filter = {email: email}
-      console.log(filter)
-      const option = {upsert: true}
+      console.log(email);
+      if(email){
+        const user = req.body;
+      const filter = { email: email };
+      const option = { upsert: true };
       const updateDoc = {
         $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, option);
+      const token = jwt.sign({ email: email }, process.env.DB_ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      const fullResult = { result, token };
+      res.send(fullResult);
       }
-      const result = await userCollection.updateOne(filter, updateDoc, option)
-      res.send(result)
-    })
+    });
   } finally {
     //lkj
   }
@@ -105,4 +129,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
